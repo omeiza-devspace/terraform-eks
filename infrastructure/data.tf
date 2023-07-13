7853dd685c6429001b903f78afebe286d3aed2d9
Okuta Omeiza <okuta.omeiza@gmail.com>
2023-07-13 17:17:59 +0100

    refactor the codebase

---
+++ infrastructure/data.tf
@@ -0,0 +1,8 @@
+# get all available AZs in our region
+data "aws_availability_zones" "available_azs" {
+  state = "available"
+}
+ 
+ # used for accesing Account ID and ARN
+data "aws_caller_identity" "current" {}
+
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,12 @@
 
 locals {
-  env      = "dev"
-  region   = "eu-west-1"
+  env            = "dev"
+  region         = "eu-west-1"
+  cluster_name   = "eks-demo"
+  available_azs  = ["${local.region}a", "${local.region}b"]
+  vpc_cidr_block = "10.0.0.0/16"
+  subnet_ext     = 4
+  zone_offset    = 8
 }
 
 #configure aws provider
@@ -12,63 +17,71 @@ provider "aws" {
 }
 
 module "network" {
-  source          = "../modules/network"
-  env             = local.env
-  vpc_cidr_block  = "10.0.0.0/16"
-  azs             = ["${local.region}a", "${local.region}b"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  private_subnets = ["10.0.5.0/24", "10.0.6.0/24"]
+  source         = "../modules/network"
+  env            = local.env
+  vpc_cidr_block = local.vpc_cidr_block
+  azs            = local.available_azs
+
+  public_subnets = [
+    for zone_id in local.available_azs:
+    cidrsubnet(local.vpc_cidr_block, local.subnet_ext, local.zone_offset)
+  ]
+
+  private_subnets = [
+    for zone_id in local.available_azs:
+    cidrsubnet(local.vpc_cidr_block, local.subnet_ext, local.zone_offset)
+  ]
 
   private_subnet_tags = {
-    "kubernetes.io/role/iternal-elb" = 1
-    "kubernetes.io/cluster/dev-demo" = "owned"
+    "kubernetes.io/role/internal-elb"             = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/role/elb"         = 1
-    "kubernetes.io/cluster/dev-demo" = "owned"
+    "kubernetes.io/role/elb"                      = 1
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
   }
 }
 
-module "eks" {
-  source = "../modules/eks"
+# module "eks" {
+#   source = "../modules/eks"
 
-  env         = local.env
-  eks_version = "1.26"
-  eks_name    = "demo"
-  subnet_ids  = module.network.private_subnet_ids
-  vpc_id      = module.network.vpc_id
+#   env         = local.env
+#   eks_version = "1.26"
+#   eks_name    = "local.cluster_name"
+#   subnet_ids  = module.network.private_subnet_ids
+#   vpc_id      = module.network.vpc_id
 
-  node_groups = {
-    general = {
-      capacity_type  = "ON_DEMAND"
-      instance_types = ["t2.micro"]
-      scaling_config = {
-        desired_size = 1
-        min_size     = 0
-        max_size     = 1
-      }
-    }
-  }
-  ssh_key_pair = "~/.ssh/terraform_ssh_key.pub"
-  depends_on   = [module.network]
-}
+#   node_groups = {
+#     general = {
+#       capacity_type  = "ON_DEMAND"
+#       instance_types = ["t2.micro"]
+#       scaling_config = {
+#         desired_size = 1
+#         min_size     = 0
+#         max_size     = 1
+#       }
+#     }
+#   }
+#   ssh_key_pair = "~/.ssh/terraform_ssh_key.pub"
+#   depends_on   = [module.network]
+# }
 
-resource "null_resource" "bashctl" {
-  depends_on = [module.eks]
+# resource "null_resource" "bashctl" {
+#   depends_on = [module.eks]
 
-  provisioner "local-exec" {
-    # connect to eks-cluster nodes
-    command     = "aws eks update-kubeconfig --region ${local.region} --name ${module.eks.eks_name}"
-    interpreter = ["/bin/bash", "-c"]
-  }
+#   provisioner "local-exec" {
+#     # connect to eks-cluster nodes
+#     command     = "aws eks update-kubeconfig --region ${local.region} --name ${module.eks.eks_name}"
+#     interpreter = ["/bin/bash", "-c"]
+#   }
 
-  provisioner "local-exec" {
-    # deploy nginx-app to eks-cluster
-    command     = "./scripts/install-nginx.sh"
-    interpreter = ["/bin/bash", "-c"]
-  }
-}
+#   provisioner "local-exec" {
+#     # deploy nginx-app to eks-cluster
+#     command     = "./scripts/install-nginx.sh"
+#     interpreter = ["/bin/bash", "-c"]
+#   }
+# }
 
 
 
